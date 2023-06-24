0a9eb98e903fdaa575d2a24c2ea13a461f910acc
esramukaddescetin <101871510+esramukaddescetin@users.noreply.github.com>
2023-06-24 17:18:04 +0300

    Project Initialized.

---
+++ ConsoleDers/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleDers;
+List<Student> studentList = new List<Student>();
+
+while (true)
+{
+    Console.WriteLine("Hoşgeldiniz!");
+    Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz!");
+    Console.WriteLine("1- Öğrenci Ekle");
+    Console.WriteLine("2- Öğrenci Sil");
+    Console.WriteLine("3- Öğrenci Listesini Görüntüle");
+    Console.WriteLine("4- Çıkış");
+    Console.Write("Seçiniz ");
+
+    string choose = Console.ReadLine();
+
+    switch (choose)
+    {
+        // öğrenci ekle
+        case "1":
+            {
+                Console.Write("Öğrenci Adı: ");
+                string isim = Console.ReadLine();
+                Console.Write("Öğrenci Soyadı: ");
+                string soyad = Console.ReadLine();
+                Console.Write("Öğrencinin Sınıfı: ");
+                char sinif = Console.ReadLine()[0];
+                Student student = new Student(isim, soyad, sinif);
+                studentList.Add(student);
+                Console.WriteLine("Öğrenci başarıyla eklendi!");
+                break;
+            }
+        //öğrenci sil
+        case "2":
+            {
+                PrintAllStudents();
+                Console.Write("Silmek istediğiniz öğrencinin numarasını giriniz: ");
+                int number = int.Parse(Console.ReadLine());
+                var foundStudent = studentList.Find(s => s.Number == number);
+                if (foundStudent != null)
+                {
+                    studentList.Remove(foundStudent);
+                    Console.Write("Öğrenci sistemden silindi!");
+                }
+                else
+                {
+                    Console.Write("Öğrenci bulunamadı!");
+                }
+                break;
+            }
+        // liste görüntüle
+        case "3":
+            {
+
+
+                if (studentList.Count == 0)
+                    Console.WriteLine("Öğrenci Listesi Boş!");
+                else
+                    PrintAllStudents();
+
+                break;
+            }
+
+        //çıkış
+        case "4": return;
+        default: choose = Console.ReadLine(); break;
+    }
+}
+
+void PrintAllStudents()
+{
+    Console.WriteLine("Öğrenci Listesi Görüntüleniyor...");
+    foreach (var student in studentList)
+    {
+        student.Print();
+    }
+    Console.WriteLine("Öğrenci Listesi Görüntülendi.");
+}
---
+++ ConsoleDers/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDers
+{
+    internal class Student
+    {
+
+        private static int _numberCounter = 0;
+        private static int NumberCounter { get { return ++_numberCounter; } }
+
+        public int Number { get; }
+
+        public string Name;
+        public string Surname;
+
+        private char _classRoom;
+        private int _classChangeCounter = 0;
+        public char ClassRoom
+        {
+            get { return _classRoom; }
+            set
+            {
+                _classChangeCounter++;
+                _classRoom = value;
+            }
+        }
+
+
+        public Student(string name, string surname, char classRoom)
+        {
+            Number = Student.NumberCounter;
+            Name = name;
+            Surname = surname;
+            _classRoom = classRoom;
+        }
+
+        public string GetFullName()
+        {
+            return Name + " " + Surname;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Öğrenci numarası: "+Number);
+            Console.WriteLine("Öğrenci tam adı: " + GetFullName());
+            Console.WriteLine("Öğrenci sınıfı: "+ClassRoom);
+            Console.WriteLine("---------------------------");
+
+        }
+
+    }
+}
